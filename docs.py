6c5e546349b4ba7963fe14ed5e266a1df15d7ee6
git-clay <ce.smith5525@gmail.com>
2017-04-09 11:37:14 -0600

    converted to python

---
+++ docs.py
@@ -0,0 +1,67 @@
+"""Documentation for pyrebase."""
+
+# WRITE DATA
+
+# push to db example
+# data = {"name": "Mortimer 'Morty' Smith"}
+# db.child("users").push(data)
+
+# create custom keys
+# data = {"name": "Mortimer 'Morty' Smith"}
+# db.child("users").child("Morty").set(data)
+
+# update
+# db.child("users").child("Morty").update({"name": "Mortiest Morty"})
+
+# delete
+# db.child("users").child("Morty").remove()
+
+# multi-location writes
+# data = {
+#     "users/"+ref.generate_key(): {
+#         "name": "Mortimer 'Morty' Smith"
+#     },
+#     "users/"+ref.generate_key(): {
+#         "name": "Rick Sanchez"
+#     }
+# }
+# db.update(data)
+
+# RETRIEVE DATA
+
+# val
+# users = db.child("users").get()
+# print(users.val()) # {"Morty": {"name": "Mortimer 'Morty' Smith"},
+# "Rick": {"name": "Rick Sanchez"}}
+
+# key
+# user = db.child("users").get()
+# print(user.key()) # users
+
+# get
+# all_users = db.child("users").get()
+
+# live change listener
+# def stream_handler(message):
+#    print(message["event"]) # put
+#    print(message["path"]) # /-K7yGTTEp7O549EzTYtI
+#    print(message["data"]) # {'title': 'Pyrebase', "body": "etc..."}
+# my_stream = db.child("posts").stream(stream_handler)
+# my_stream.close()
+
+# COMPLEX QUERIES
+
+# example
+# users_by_name = db.child("users").order_by_child("name").limit_to_first(3).get()
+
+# equal to
+# users_by_score = db.child("users").order_by_child("score").equal_to(10).get()
+
+# start at end at
+# users_by_score = db.child("users").order_by_child("score").start_at(3).end_at(10).get()
+
+# order by key
+# users_by_key = db.child("users").order_by_key().get()
+
+# order by value
+# users_by_value = db.child("users").order_by_value().get()
---
+++ seed.py
@@ -0,0 +1,69 @@
+"""File to seed firebase."""
+# https://github.com/thisbejim/Pyrebase
+import pyrebase
+import os
+import requests
+import json
+import urllib
+from flask import make_response
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+apiKey = os.environ.get("apiKey")
+authDomain = os.environ.get("authDomain")
+databaseURL = os.environ.get("databaseURL")
+storageBucket = os.environ.get("storageBucket")
+project_id = os.environ.get("project_id")
+token = os.environ.get("token")
+config = {
+    "apiKey": apiKey,
+    "authDomain": authDomain,
+    "databaseURL": databaseURL,
+    "storageBucket": storageBucket
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+
+def templates_dir():
+    template_dir = "var/www/loader/loader/templates/"
+    template_dir = "templates/"
+    return template_dir
+
+
+def build_url(command):
+    """Create url to access kinetise."""
+    host = "https://api-cms-fitrock.kinetise.com/"
+    query = host + "api/kinetise/v2/projects/" + project_id + "/" + command + "access_token=" + token
+    return query
+
+
+def get_deal(deal_id):
+    """Get deal."""
+    query = build_url("tables/1/rows?id=" + deal_id + "&")
+    res = requests.get(query)
+    t_dir = templates_dir()
+    o = res.json()
+    row = o[0]
+    try:
+        category = row['category']['related_rows']
+    except:
+        category = 0
+
+    context = {
+        'title': 'WalkTo Deal',
+        'description': row['description'],
+        'openinghours': row['openinghours'],
+        'picture': row['picture'],
+        'address': row['address'],
+        'latitude': row['latitude'],
+        'longitude': row['longitude'],
+        'venue_title': row['title'],
+        'id': row['id'],
+        'author_id': row['_author_id'],
+        'category': category,
+    }
+    return context
+
+print (get_deal('270'))
